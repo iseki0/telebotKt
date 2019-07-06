1ff170b0b741d0bfee9d9cbb70775f5ae24e88cb
iseki <admin@iseki.space>
2019-07-06 22:36:37 +0800

    clean up code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,9 @@
 import io.vertx.core.json.JsonObject
 
 fun main() {
-    println(JsonObject(mapOf(Pair("a", null)).filterNot { it.value == null }))
-
-
+    println("Hello world.")
 }
 
-fun aa(a: String): JsonObject = JsonObject(mapOf(Pair("a", 5)))
 
 fun genBotRequest(api: String, vararg pairs: Pair<String, Any?>): BotRequest =
     BotRequest(api, JsonObject(mapOf(*pairs).filterNot { it.value == null }))
--- src/main/kotlin/types/Main.kt
+++
@@ -1,5 +0,0 @@
-package types
-
-fun main() {
-    println("Hello world!!!")
-}
