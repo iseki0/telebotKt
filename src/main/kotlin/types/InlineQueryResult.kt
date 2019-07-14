73b91440fdbcc8b332188a37225575311fed74dd
iseki <admin@iseki.space>
2019-07-14 18:11:01 +0800

    InlineQueryResult never been deserialized. Needn't custom deserializer.

--- src/main/kotlin/types/InlineQueryResult.kt
+++ src/main/kotlin/types/InlineQueryResult.kt
@@ -3,5 +3,6 @@ package types
 /**
  * This object represents one result of an inline query. Telegram clients currently support results of the following 20 types:
  *
+ * NOTE: Never been deserialized. Needn't custom deserializer.
  */
 interface InlineQueryResult
