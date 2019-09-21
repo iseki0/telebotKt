1d72031badfcb48f1093b18fafc175679e55fe1e
iseki <admin@iseki.space>
2019-09-21 21:43:16 +0800

    archive.

--- src/main/kotlin/ApiContext.kt
+++ src/main/kotlin/ApiContext.kt
@@ -1,5 +1,7 @@
 import api.ResultType
 import io.vertx.core.Future
+import java.math.BigInteger
+import kotlin.system.measureTimeMillis
 
 interface ApiContext {
     fun <T : ResultType> doSendRequest(
@@ -7,4 +9,17 @@ interface ApiContext {
         args: List<Pair<String, Any?>>,
         resultType: Class<T>
     ): Future<T>
+}
+
+fun main(){
+    var a=BigInteger("0")
+    var b=BigInteger("1")
+    measureTimeMillis {
+        repeat(10_0000){
+            val t=a
+            a=b
+            b=a+t
+        }
+    }.apply { println(this) }
+    println(b)
 }
